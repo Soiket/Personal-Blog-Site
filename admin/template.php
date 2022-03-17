95bd5001cdf1b3e0bcfa518a905520d33bf42383
soikot <misoiket@gmail.com>
2022-03-17 14:06:09 +0600

    navbar dynamic

---
+++ admin/template.php
@@ -0,0 +1,35 @@
+<?php include_once("includes/header.php") ?>
+
+<body class="sb-nav-fixed">
+    <?php include_once("includes/topnav.php") ?>
+    <div id="layoutSidenav">
+        <?php include_once("includes/sidenav.php") ?>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid">
+
+                    <?php
+                    if (isset($view)) {
+                        if ($view == "dashboard") {
+                            include("view/dashboard_view.php");
+                        } elseif ($view == "add_post") {
+                            include("view/add_postview.php");
+                        } elseif ($view == "manage_post") {
+                            include("view/manage_postview.php");
+                        } elseif ($view == "add_category") {
+                            include("view/add_categoryview.php");
+                        } elseif ($view == "manage_category") {
+                            include("view/manage_categoryview.php");
+                        }
+                    }
+
+                    ?>
+                </div>
+            </main>
+            <?php include_once("includes/footer.php") ?>
+        </div>
+    </div>
+    <?php include_once("includes/script.php") ?>
+</body>
+
+</html>
